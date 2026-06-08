7803eaf819515fea61c0cc7ef49e1c49a9584444
julio-urquiza <julio.cesar.uquiza4@gmail.com>
2026-06-08 12:50:42 -0300

    fix: refine CORS configuration to allow specific origins

--- src/app.js
+++ src/app.js
@@ -7,11 +7,22 @@ import passport from "./config/passport.jwt.js"
 
 const app = express()
 
-// app.use(cors({
-//   origin: ["http://localhost:5173", "https://sportconnectg.netlify.app/"],
-//   credentials: true
-// }))
-app.use(cors())
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://sportconnectg.netlify.app"
+]
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true
+}))
+// app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
